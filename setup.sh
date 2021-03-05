2dba616fe8be4544471d2ea1b5b5ecbe77b9c2d0
codetox <axel_lidy@outlook.fr>
2021-03-05 12:17:28 +0100

    ft_services pue la merde

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-minikube start --vm-driver=virtualbox --memory=3g
+#!/bin/sh
+
+minikube start --vm-driver=virtualbox --memory=3g 
 eval $(minikube docker-env)
-minikube addons enable metrics-server
-minikube addons enable dashboard
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 docker build -t nginx-image ./services/Nginx
 docker build -t mysql-image ./services/MySQL
@@ -11,4 +11,6 @@ docker build -t phpmyadmin-image ./services/PhpMyAdmin
 docker build -t wordpress-image ./services/WordPress
 docker build -t ftps-image ./services/Ftps
 kubectl apply -k .
+minikube addons enable metrics-server
+minikube addons enable dashboard
 minikube dashboard
